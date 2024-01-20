c815a80b070c32600933b433b605ecf78db44b26
Brijesh Wawdhane <brijesh@wawdhane.com>
2024-01-20 23:27:41 +0530

    add gcp vm ssh script

---
+++ gcp-vm-ssh-setup.sh
@@ -0,0 +1,26 @@
+# don't allow password login
+sudo sed -i 's/PermitRootLogin no/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+
+# create authorized_keys file
+sudo mkdir /root/.ssh
+sudo chmod 700 /root/.ssh
+sudo touch /root/.ssh/authorized_keys
+sudo chmod 600 /root/.ssh/authorized_keys
+
+# add my public key to authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXdAzn53i+OygyKItq8TF4GSW0cS7g24cOHVKEkuR4Z brijesh@wawdhane.com" >> /root/.ssh/authorized_keys
+
+# install zsh
+sudo apt update
+sudo apt install zsh
+
+# make zsh the default shell
+chsh -s $(which zsh)
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+source ~/.zshrc
+
+# restart sshd
+sudo systemctl restart sshd
