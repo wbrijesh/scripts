380d6e80cf5d60b2f2898592d95574a18e356370
Brijesh Wawdhane <ops@brijesh.dev>
2026-01-29 08:26:08 +0530

    Modify gum installation prompt handling
    Change error message output to stderr and read from tty.

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,8 +45,9 @@ gpgkey=https://repo.charm.sh/yum/gpg.key' | sudo tee /etc/yum.repos.d/charm.repo
 
 # Check if gum is installed
 if ! command -v gum &> /dev/null; then
-    echo "Installer has unmet dependencies."
-    read -p "Would you like to install them? (y/n) " -n 1 -r < /dev/tty
+    echo "Installer has unmet dependencies." >&2
+    exec < /dev/tty
+    read -p "Would you like to install them? (y/n) " -n 1 -r
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         install_gum
